59ddb07f51f238eac6b35cd05438605fc088db64
Carly <cmiranda@ucol.mx>
2018-04-10 18:55:03 -0500

    Agregué casos de prueba

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -14,21 +14,27 @@ float pago_estacionamiento(int horas)
 		cant = horas*10;
 	if (horas>=6 && horas<=10)
 		cant = horas*9;
-	if (horas>11)
+	if (horas>10)
 		cant = horas*7;
 
 	return cant;
 }
 
 void casos_de_prueba(){
+	float pago[5]={pago_estacionamiento(-1), pago_estacionamiento(3), pago_estacionamiento(6),pago_estacionamiento(17)};
+	string verificacion[4];
+	verificacion[0]= (pago[0]==-1)? "Pass":"Fail";
+	verificacion[1]= (pago[1]==30)? "Pass":"Fail";
+	verificacion[2]= (pago[2]==54)? "Pass":"Fail";
+	verificacion[3]= (pago[3]==119)? "Pass":"Fail";
 
     cout << "TEST CASES" << endl;
-    // Agrega tus casos de prueba
-
-
-
+    cout << "Test data  Expected  Actual  Pass/Fail"<<endl;
+    cout << "-1 hora      ERROR    " << pago[0] << "       " <<verificacion[0] << endl;
+    cout << "3 horas      30       " << pago[1] << "      " <<verificacion[1] << endl;
+    cout << "6 horas      54       " << pago[2] << "      " <<verificacion[2] << endl;
+    cout << "17 horas     119      " << pago[3] << "     " <<verificacion[3] << endl;
     cout << endl;
-
 }
 
 int main()
